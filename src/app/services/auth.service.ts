44cbbcabe57ae4bea8792e8d78505f1038becfaa
lallen30 <lallen30@yahoo.com>
2020-04-20 12:53:44 -0400

    Add auth files

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { BehaviorSubject } from 'rxjs';
+
+const apiUrl = environment.apiUrl;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  isAuthenticated = new BehaviorSubject<boolean>(false);
+
+  constructor(private http: HttpClient) { }
+
+  postLogin(data) {
+    return this.http.post(`${apiUrl}jwt-auth/v1/token`, data);
+  }
+}
